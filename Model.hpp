64287c5a9196071d8ebdf5a2c1a701ae459a81d5
qb <1215181q@gmail.com>
2022-07-06 11:07:05 +0430

    Switch Keys Updated

---
+++ Model.hpp
@@ -0,0 +1,15 @@
+#ifndef MODEL_HPP
+#define MODEL_HPP
+
+#include "Configs.hpp"
+
+class Model {
+    private:
+        double modelX;
+        double modelY;
+        double modelTetha;
+    public:
+
+};
+
+#endif // MODEL_HPP
